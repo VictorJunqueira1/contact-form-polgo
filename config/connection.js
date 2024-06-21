5ae055a6c2d37672ae09c849dde24cb6d307e2ad
VictorJunqueira1 <victorjunqueira.prog@gmail.com>
2024-06-21 16:40:09 -0300

    feat: Adicionando requisição com o banco de dados.

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose"
 import * as dotenv from "dotenv"
 
 dotenv.config()
-main().then(() => { console.log("MongoDB rodando com sucesso.") }).catch(err => console.log(err));
 
 async function main() {
   if (process.env.MONGO_USER !== "" && process.env.MONGO_PASSWORD !== "") {
     await mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`);
   }
   await mongoose.connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`);
-
+  
 }
+main().then(() => { console.log("MongoDB rodando com sucesso.") }).catch(err => console.log(err));
--- server.js
+++ server.js
@@ -1,52 +1,55 @@
-import e, { json } from "express"
-import * as dotenv from "dotenv"
-import "./config/connection.js"
-import { Contact } from "./models/Contact.js"
-dotenv.config()
+import express from "express";
+import * as dotenv from "dotenv";
+import "./config/connection.js";
+import { Contact } from "./models/Contact.js";
+import cors from 'cors';
+dotenv.config();
 
-const app = e()
-const PORT = 3000
-app.use(json())
-console.log(process.env.MONGO_PORT)
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(cors());
+
+console.log(process.env.MONGO_PORT);
 
 app.post("/submit", async (request, response) => {
-    const { name, email, message } = request.body
-    
-    if (name.length < 3) {
-        return response.status(411).json({
-            error: "Insira um nome com no mínimo 3 caracteres."
-        })
-    } 
+    try {
+        const { name, email, message } = request.body;
+        if (name.length < 3) {
+            return response.status(411).json({
+                error: "Insira um nome com no mínimo 3 caracteres."
+            });
+        }
 
-    if (message.length < 10) {
-        return response.status(411).json({
-            error: "Insira no mínimo 10 caracteres na mensagem."
-        })
-    } 
+        if (message.length < 10) {
+            return response.status(411).json({
+                error: "Insira no mínimo 10 caracteres na mensagem."
+            });
+        }
 
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    if (!regex.test(email)) {
-        return response.status(406).json({
-            error: "Insira um e-mail válido."
-        })
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!regex.test(email)) {
+            return response.status(406).json({
+                error: "Insira um e-mail válido."
+            });
+        }
+
+        const contact = new Contact();
+        const contactCollection = contact.db.collection("contacts");
+        await contactCollection.insertOne({ name, email, message });
+        console.log("Mensagem inserida no banco de dados");
+        
+        return response.status(200).json({
+            message: "Mensagem do usuário enviada com sucesso."
+        });
+    } catch (error) {
+        console.error("Erro ao processar a requisição:", error);
+        return response.status(500).json({
+            error: "Ocorreu um erro ao processar a sua requisição."
+        });
     }
-
-    /* 
-        Passo 1: Pegar valor do email 
-        Passo 2: Verificar se o email já existe no banco de dados
-        Passo 3: Se existir, não deixar inserir
-        Passo 4: Se não existir, registrar
-    */
-
-    const contact = new Contact()
-    const contactCollection = await contact.db.createCollection("contacts");
-    contactCollection.insertOne();
-
-    return response.status(200).json({
-        message: "Mensagem do usuário enviada com sucesso."
-    })
-})
+});
 
 app.listen(PORT, () => {
-    console.log("Servidor rodando na porta: " + PORT)
-}) 
+    console.log("Servidor rodando na porta: " + PORT);
+});
